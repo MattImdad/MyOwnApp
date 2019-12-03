1f54aea17c2d4801a24de49fdee549a383dbb362
matt imdad <mimdad@bsd20.org>
2019-12-03 10:34:54 -0600

    Initial Commit

---
+++ MyOwnApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOwnApp
+//
+//  Created by matt imdad on 12/3/19.
+//  Copyright © 2019 matt imdad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
